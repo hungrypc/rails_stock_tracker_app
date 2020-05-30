93ebb5dc33bbdc1cce790c4d446d88899609d529
hpc <philipchanhk626@gmail.com>
2020-05-30 14:43:41 -0400

    storing credentials not working, leaving api open whatever

---
+++ app/models/stock.rb
@@ -0,0 +1,10 @@
+class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_a3206175d4ea446dbe4e0350061c2b25',  # for some reason, i cant store secrets
+      secret_token: 'Tsk_4e0d2d1a1f8842a7b1966abe523af68dn',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price(ticker_symbol)
+  end
+end
