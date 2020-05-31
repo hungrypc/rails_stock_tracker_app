52d3639506fff58ea1f5dfed9602d8cc2f419c2f
hpc <philipchanhk626@gmail.com>
2020-05-31 14:57:44 -0400

    stock display now a partial, handling ajax requests

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,7 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
