07ac37e879a9ab32683333ad28fcda3be35e72a7
hpc <philipchanhk626@gmail.com>
2020-05-31 14:34:24 -0400

    handling stock search edge cases

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
       secret_token: 'Tsk_4e0d2d1a1f8842a7b1966abe523af68dn',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
