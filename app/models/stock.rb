6796810fa8cf8844ad16e7e96c6dccb3c33df0b3
hpc <philipchanhk626@gmail.com>
2020-06-01 16:26:47 -0400

    able to add stocks to portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker_symbol, presence: true
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -15,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    Stock.where(ticker: ticker_symbol).first
   end
 end
