2379855bd25625025e1ddac5e463968f8b033117
hpc <philipchanhk626@gmail.com>
2020-06-01 14:50:10 -0400

    added stock listings, made partial

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,9 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
-  
+
+  validates :name, :ticker_symbol, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: 'Tpk_a3206175d4ea446dbe4e0350061c2b25',  # for some reason, i cant store secrets
