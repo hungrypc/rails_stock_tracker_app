a47c3d7d70a19cf30643602b63c76c87bcbbebae
hpc <philipchanhk626@gmail.com>
2020-06-03 09:14:53 -0400

    users can now follow and unfollow friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,8 @@ class User < ApplicationRecord
   def except_current_user(users)
     users.reject { |user| user.id == self.id }
   end
+
+  def not_friends_with?(id)
+    !self.friends.where(id: id).exists?
+  end
 end
